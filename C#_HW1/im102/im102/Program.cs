8d5815873e03bd1e564da0b4f6e4d5624a782c50
Mayishdev <142711918+Mayishdev@users.noreply.github.com>
2025-09-09 21:16:06 +0800

    Added C#_HW1 folder

---
+++ C#_HW1/im102/im102/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace im102
+{
+    class Program
+    {
+     
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter string: ");
+                string instri = Console.ReadLine();
+
+                if (instri == "*")
+                {
+                    Console.WriteLine("Invalid Input!!!!");
+                    break;
+                }
+
+                string original = instri;
+                string lowerInput = instri.ToLower();
+
+                string reversed = "";
+                for (int i = lowerInput.Length - 1; i >= 0; i--)
+                {
+                    reversed += lowerInput[i];
+                }
+
+                
+                string result = (lowerInput == reversed) ? "Yes" : "No";
+
+                Console.WriteLine($"Original String: {original}");
+                Console.WriteLine($"Reversed String: {reversed}");
+                Console.WriteLine($"Palindrome? {result}");
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
